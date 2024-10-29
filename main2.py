c5347c0b05652647e68f03a5bf051beebd3eb3aa
PedroJosePintoRivadeneira <186082557+PedroJosePintoRivadeneira@users.noreply.github.com>
2024-10-29 11:58:36 -0500

    Tipos de datos en python

---
+++ main2.py
@@ -0,0 +1,16 @@
+fullName = "Pedro Jose Pinto Rivadeneira" #Tipo de dato script
+gender = "M" #Tipo de dato char
+age = 23 #Tipo de dato int (enteros)
+height = 1.65 #Tipo de dobule (decimales con punto, no coma)
+camper = True #Tipo de dato boolean
+
+# Conjuntos
+hobbies = ["Anime", "Rpg game", "Assasin"] #Tipo de dato Array
+direction = (dict) ["country": "Colombia", "state": "Santander"] #Tipo de dato object
+
+print(f""" Student's Name: "{2+2}" """)
+
+
+  #Cosas que no debe hacer
+ #No combinar variables
+ #Crear variables con numeros name 1, funciona name_1. no es recomendable
