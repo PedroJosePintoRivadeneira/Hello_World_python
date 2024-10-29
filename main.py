8b82024053a6432e223a9f8d68e6ca297a762a95
PedroJosePintoRivadeneira <186082557+PedroJosePintoRivadeneira@users.noreply.github.com>
2024-10-29 12:23:07 -0500

    Ejercicio Python 2

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-name = input("Enter your name; ")
-message = f"Hello World {name}"
-print(message)
+fullName = input("Enter your name ")
+age = int(input("Enter your age "))
+gender = input("Enter your possibily gender M, F,X: ")
+height = input("Enter your height ")
+
+print(f""" 
+Hola usuario {fullName}. ¿Como estas hoy?.
+No sabia que eras un humano tipo {gender} con una altura de {height}
+Gracias por la informacion""")
